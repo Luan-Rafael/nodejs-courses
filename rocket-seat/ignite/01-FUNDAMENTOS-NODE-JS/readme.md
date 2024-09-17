f107f0182963b19746d1601ff5513f12df144736
Luan Rafael <luan.tec18@gmail.com>
2024-09-16 22:00:21 -0300

    publishing projects made in node

---
+++ rocket-seat/ignite/01-FUNDAMENTOS-NODE-JS/readme.md
@@ -0,0 +1,13 @@
+# Course of NodeJs
+
+# GET POST PUT PATCH DELETE
+# GET => buscar um recurso
+# POST => criar um recurso
+# PUT => atualizar um recurso
+# PATCH => atualizar uma informaçõa específica de um recurso
+# DELETE => deletar um recurso
+# STATEFULL - STATELESS
+# APLICAÇÃO STATEFULL => dados da aplicação são salvos em memória
+# APLICAÇÃO STATELESS => dados da aplicação são salvos em dispositivos externos como (banco de dados)
+# Cabeçalhos (requisição;resposta) => MetaDados
+# HTTP STATUS CODE
---
+++ rocket-seat/ignite/01-FUNDAMENTOS-NODE-JS/src/database.js
@@ -0,0 +1,44 @@
+import fs from 'node:fs/promises'
+
+const dataBasePath = new URL('../db.json', import.meta.url)
+
+export class Database {
+    #database = {}
+
+    constructor() {
+        fs.readFile(dataBasePath, 'utf8')
+            .then((data) => {
+                this.#database = JSON.parse(data)
+            })
+            .catch(() => {
+                this.#persist()
+            })
+    }
+
+    #persist() {
+        fs.writeFile(dataBasePath, JSON.stringify(this.#database))
+    }
+
+    select(table) {
+        const data = this.#database[table] ?? []
+        return data
+    }
+    insert(table, data) {
+        if (Array.isArray(this.#database[table])) {
+            this.#database[table].push(data)
+        } else {
+            this.#database[table] = [data]
+        }
+
+        this.#persist()
+    }
+    delete(table, id) {
+        const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+        if (rowIndex >= 0) {
+            this.#database[table].splice(rowIndex, 1)
+            this.#persist()
+        }
+
+    }
+}
